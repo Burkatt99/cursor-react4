055b759d400b32f527e68fa9b58f1f2416289f28
Таня Бура <tanya.bura.99@gmail.com>
2020-04-15 20:31:26 +0300

    Updates

---
+++ precache-manifest.9a987cf97fda616e85924f764f19c6e2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c5dee72b57d19ce645a7937a48897f",
+    "url": "/cursor-react4/index.html"
+  },
+  {
+    "revision": "4d1d1f447d253db8ca49",
+    "url": "/cursor-react4/static/css/main.fd5df401.chunk.css"
+  },
+  {
+    "revision": "e53e3ba9569815a268b1",
+    "url": "/cursor-react4/static/js/2.5b393ca2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-react4/static/js/2.5b393ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1d1f447d253db8ca49",
+    "url": "/cursor-react4/static/js/main.c6fe450c.chunk.js"
+  },
+  {
+    "revision": "aeb095a22d0ebddc70fa",
+    "url": "/cursor-react4/static/js/runtime-main.5c2b6730.js"
+  },
+  {
+    "revision": "54ce72e640f94b7a6bfb6646f4c49141",
+    "url": "/cursor-react4/static/media/1.54ce72e6.jpg"
+  },
+  {
+    "revision": "4c5146bda596269ce2d001fcda485056",
+    "url": "/cursor-react4/static/media/10.4c5146bd.jpg"
+  },
+  {
+    "revision": "6689d1605eb3fc468bcd30bb2c680be3",
+    "url": "/cursor-react4/static/media/11.6689d160.jpg"
+  },
+  {
+    "revision": "f01fa26084ca147ed75b60073b72dcd0",
+    "url": "/cursor-react4/static/media/12.f01fa260.jpg"
+  },
+  {
+    "revision": "2734df4b20f5e9a3b117dedc78824ff2",
+    "url": "/cursor-react4/static/media/13.2734df4b.jpg"
+  },
+  {
+    "revision": "477597f9544ff65b8e1477b8c8f820c7",
+    "url": "/cursor-react4/static/media/14.477597f9.jpg"
+  },
+  {
+    "revision": "3124fe02618130bb38219b3d6e9a2c52",
+    "url": "/cursor-react4/static/media/15.3124fe02.jpg"
+  },
+  {
+    "revision": "785cbddd1f661853e34276359c93de78",
+    "url": "/cursor-react4/static/media/2.785cbddd.jpg"
+  },
+  {
+    "revision": "ca63c168a37c1838f0007175b5415813",
+    "url": "/cursor-react4/static/media/3.ca63c168.jpg"
+  },
+  {
+    "revision": "d9123792caffba985a6c5fdbc91b8c98",
+    "url": "/cursor-react4/static/media/4.d9123792.jpg"
+  },
+  {
+    "revision": "6b7639091eadba78288d4168d4e2050e",
+    "url": "/cursor-react4/static/media/5.6b763909.jpg"
+  },
+  {
+    "revision": "1fa79b669996b4ff062384779b68fef3",
+    "url": "/cursor-react4/static/media/6.1fa79b66.jpg"
+  },
+  {
+    "revision": "e611e417ee4f77a1b0c530163c8427f4",
+    "url": "/cursor-react4/static/media/7.e611e417.jpg"
+  },
+  {
+    "revision": "9b62d85095e8a96d82fb2d0b9f83589a",
+    "url": "/cursor-react4/static/media/8.9b62d850.jpg"
+  },
+  {
+    "revision": "bc39ab962509ed84f5f371e982b735ae",
+    "url": "/cursor-react4/static/media/9.bc39ab96.jpg"
+  },
+  {
+    "revision": "961b91b5bb03db62d44bd2f429b795dd",
+    "url": "/cursor-react4/static/media/author.961b91b5.jpg"
+  },
+  {
+    "revision": "32bc175e20a604ccb654f7682dbd0b00",
+    "url": "/cursor-react4/static/media/books.32bc175e.jpg"
+  },
+  {
+    "revision": "9251733ad0ff0cf5ab0b194c00859372",
+    "url": "/cursor-react4/static/media/dog.9251733a.png"
+  },
+  {
+    "revision": "8e1199e9ce41a9d7741c3989cee4a5ac",
+    "url": "/cursor-react4/static/media/fog.8e1199e9.jpg"
+  },
+  {
+    "revision": "b94d21f4e34cee22064671d2cd88f556",
+    "url": "/cursor-react4/static/media/loupe.b94d21f4.png"
+  },
+  {
+    "revision": "055b64e73b010f6952737f27cd401177",
+    "url": "/cursor-react4/static/media/male.055b64e7.png"
+  },
+  {
+    "revision": "aec20cdb3145c59c3879e9eac21de353",
+    "url": "/cursor-react4/static/media/man.aec20cdb.jpg"
+  },
+  {
+    "revision": "6038ae13d70b3100bd06334f1eae9eab",
+    "url": "/cursor-react4/static/media/pazzle.6038ae13.jpg"
+  },
+  {
+    "revision": "9e63954cd1ec2056a5e40f515f91fe84",
+    "url": "/cursor-react4/static/media/phone-call.9e63954c.png"
+  },
+  {
+    "revision": "529ce1785b208896c6e3a12737c10fe9",
+    "url": "/cursor-react4/static/media/po.529ce178.png"
+  }
+]);
